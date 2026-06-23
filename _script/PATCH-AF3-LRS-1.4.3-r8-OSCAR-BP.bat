ce0d622712fe070f0a63cd56560c5fc221b6ba49
mrgee-host <c.messi710@yahoo.com>
2026-06-23 16:40:31 +0700

    Update Kodi repository 2026-06-23 16:40:31

--- _script/PATCH-AF3-LRS-1.4.3-r8-OSCAR-BP.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0PATCH-AF3-LRS-1.4.3-r8-OSCAR-BP.ps1"
-echo.
-pause
--- _script/PATCH-AF3-LRS-1.4.4-r9-OSCARS-BP.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0PATCH-AF3-LRS-1.4.4-r9-OSCARS-BP.ps1"
-echo.
-pause
